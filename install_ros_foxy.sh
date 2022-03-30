b87bdadd17348388386646b4c8839325c3a7a2b9
Neoplanetz <neoplanetz@gmail.com>
2022-03-30 13:17:50 +0900

    fixed syntax error and added rosdep install

--- install_ros_foxy.sh
+++ install_ros_foxy.sh
@@ -31,7 +31,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -
 
 echo "[Add the ROS2 repository]"
 if [ ! -e /etc/apt/sources.list.d/ros2.list ]; then
-  sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+  sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
 fi
 
 echo "[Update the package lists]"
@@ -46,13 +46,20 @@ sudo apt install -y build-essential cmake git libbullet-dev python3-colcon-commo
 python3 -m pip install -U argcomplete flake8-blind-except flake8-builtins flake8-class-newline flake8-comprehensions flake8-deprecated flake8-docstrings flake8-import-order flake8-quotes pytest-repeat pytest-rerunfailures pytest
 sudo apt install --no-install-recommends -y libasio-dev libtinyxml2-dev libcunit1-dev
 
-echo "[Make the ros2 workspace and git clone ros2 example and colcon build test]"
+echo "[Make the ros2 workspace and git clone ros2 example]"
 mkdir -p $HOME/$name_colcon_workspace/src
 cd $HOME/$name_colcon_workspace/src
 git clone https://github.com/ros2/examples
 cd examples
 git checkout $ROS_DISTRO
 
+echo "[Install dependencies using rosdep]"
+cd $HOME/$name_colcon_workspace
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-5.3.1 urdfdom_headers"
+
+echo "[Colcon build test]"
 cd $HOME/$name_colcon_workspace
 colcon build --symlink-install
 
@@ -121,6 +128,7 @@ sudo apt install apt-transport-https -y
 sudo apt update
 sudo apt install code -y
 
+source $HOME/.bashrc
 source $HOME/.bashrc
 
 echo "[Complete!!!]"
